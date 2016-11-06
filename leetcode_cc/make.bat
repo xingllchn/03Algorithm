11cf28f4c90f1149e0fd12fba192bc01743dff24
xingllchn <xingllchn@163.com>
2016-11-06 23:14:54 +0800

    add 03algorithm 20161106 2314

---
+++ leetcode_cc/make.bat
@@ -0,0 +1,7 @@
+echo off
+set PATH=D:\Program Files (x86)\mingw-w64\i686-6.2.0-posix-dwarf-rt_v5-rev0\mingw32\bin;%PATH%;
+rem echo %PATH%
+rem cd "F:\03algorithm\leetcode_c"
+cmd /k "cd /d F:\03algorithm\leetcode_c"
+
+
