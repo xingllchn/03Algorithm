3de092dff4580023945dd5dabdc5bede9ccc2b86
xingllchn <xingllchn@163.com>
2016-11-13 22:01:17 +0800

    update 05_longestPalindrome

--- leetcode_cc/src/04_findMedianSortedArrays.c
+++ leetcode_cc/src/04_findMedianSortedArrays.c
@@ -9,6 +9,8 @@
 			nums2 = [3, 4]
 			The median is (2 + 3)/2 = 2.5
 */
+#include <stdio.h>
+#include <stdlib.h>
 int Min(int a, int b) 
 {    
 	return a < b ? a : b;
@@ -17,7 +19,7 @@ int Min(int a, int b)
 int FindKth(int *nums1, int n1, int *nums2, int n2, int k)
  {    
 	int pa = Min(n1, k / 2), pb = k - pa;   
-	//pa，pb分别指向两个数组的第k/2个元素，数组一小于k/2个时pa指向最后一个元素    
+	//pa，pb分别指向两个数组的第k/2个元素，数组一小于k/2个时pa指向最后一个元素
 	if(n2 < n1) //确保第一个数组短于第二个数组
 		return FindKth(nums2, n2, nums1, n1, k);    
 	if(n1 == 0) //基准情况，第一个数组为空时直接返回第二个数组中第k个元素
@@ -45,4 +47,13 @@ double findMedianSortedArrays(int* nums1, int nums1Size, int* nums2, int nums2Si
 		return FindKth(nums1, nums1Size, nums2, nums2Size, k + 1);    
 	else//总数为偶数，返回第k、第k+1个的平均数
 		return (FindKth(nums1, nums1Size, nums2, nums2Size, k) + FindKth(nums1, nums1Size, nums2, nums2Size, k + 1)) / 2.0;
+}
+int main(int argc,char ** argv)
+{
+	int nums1[2]={1,5};
+	int nums2[2]={6,8};
+	double median=0.0;
+	median=findMedianSortedArrays(nums1,sizeof(nums1)/sizeof(int),nums2,sizeof(nums2)/sizeof(int));
+	printf(" median=%f\n",median);
+	return 0;
 }
---
+++ leetcode_cc/src/05_longestPalindrome.c
@@ -0,0 +1,82 @@
+/*
+From			:	https://leetcode.com/problemset/algorithms/
+to			:	https://github.com/xingllchn/03algorithm/tree/master/leetcode_cc/src
+Data			:	2016-11-13
+author		:	xingllchn
+Description	: 
+			Example:
+				Input: "babad"
+				Output: "bab"
+			Example:
+				Input: "cbbd"
+				Output: "bb"
+知识点:
+	calloc在动态分配完内存后，自动初始化该内存空间为零，而malloc不初始化，里边数据是随机的垃圾数据。
+核心思想:
+	1)回文分奇偶情况
+	2)沿着基准i左右搜索，查找是否有相等的两个字符
+	3)若有相等，则更新start和maxLen的值，最终将字符数组的第start开始，长度为maxLen的子字符串输出。
+*/
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+char *s1;
+char * longestPalindrome(char *  s)
+{
+	int n = strlen(s);
+	char *s2;
+	int start = 0, maxLen = 1; 
+	int i, j, k ;
+	s1 =(char *)calloc(n,sizeof(char));
+	if(!s1)
+		return NULL;
+	s2 =s1;
+	for (i = 0; i < n; i++) 
+	{
+		//情况1:回文为偶数
+	    	j = i, k = i + 1; 
+	    	while (j >= 0 && k < n) 
+		{
+		        if (s[j] != s[k]) 
+					break;
+		        if (k - j + 1 > maxLen) 
+			{
+		            start = j;
+		            maxLen = k - j + 1;
+		        }
+		        j--;
+		        k++;
+	    	}
+		//情况2:回文为奇数
+	    	j = i - 1, k = i + 1; 
+	    	while(j >= 0 && k <= n - 1) 
+		{
+		        if (s[j] != s[k]) 
+					break;
+		        if (k - j + 1 > maxLen) {
+		            start = j;
+		            maxLen = k - j + 1;
+		        }
+		        j--;
+		        k++;
+	    	}
+	}
+	while(maxLen--)
+	{
+		*s1++=s[start];
+		start++;
+	}
+	return s2;
+}
+int main() {
+	char * str0 = {"aabac"};
+	char * str1 = {"abcb"};
+	char *s;
+	s=longestPalindrome(str0);
+	free(s1);
+	printf("%s\n",s);
+	s=longestPalindrome(str1);
+	free(s1);
+	printf("%s\n",s);
+	return 0;
+}
