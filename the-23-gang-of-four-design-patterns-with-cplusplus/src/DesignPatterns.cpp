802afbf9a4ae40835fb29168109b595505c5414d
xingllchn <xingllchn@163.com>
2019-12-18 23:58:54 +0800

    测试例程

---
+++ the-23-gang-of-four-design-patterns-with-cplusplus/src/DesignPatterns.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : DesignPatterns.cpp
+// Author      : xingll
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
